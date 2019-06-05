748e11b9b8be0cdbe21352193b93484824c61d9f
Jefry Sastre <jefry.sastre@gmail.com>
2019-06-05 14:28:26 -0300

    update package requirements

--- setup.py
+++ setup.py
@@ -16,16 +16,16 @@ setup(name='FlaskAPI',
         'Flask-HTTPAuth==3.2.4',
         'Flask-RESTful==0.3.7',
         'gunicorn==19.9.0',
-        'httplib2==0.12.3'
-        'itsdangerous==1.1.0'
-        'Jinja2==2.10.1'
-        'jsonpickle==0.9.5'
-        'MarkupSafe==1.1.1'
-        'peewee==3.9.3'
-        'PyJWT==1.7.1'
-        'PyMySQL==0.9.3'
-        'pytz==2019.1'
-        'six==1.12.0'
+        'httplib2==0.12.3',
+        'itsdangerous==1.1.0',
+        'Jinja2==2.10.1',
+        'jsonpickle==0.9.5',
+        'MarkupSafe==1.1.1',
+        'peewee==3.9.3',
+        'PyJWT==1.7.1',
+        'PyMySQL==0.9.3',
+        'pytz==2019.1',
+        'six==1.12.0',
         'Werkzeug==0.14.1'
       ],
       zip_safe=False)
